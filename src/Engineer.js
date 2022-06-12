3ec468144f8a52bcf51ab5d5dd2ddd3488b66dec
Alexandru <T_alexandru87@yahoo.com>
2022-06-12 14:40:56 +0100

    completed classes structure

---
+++ src/Engineer.js
@@ -0,0 +1,16 @@
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+  constructor(name, id, email, githubUsername) {
+    super(name, id, email);
+
+    this.githubUsername = githubUsername;
+    this.role = "Engineer";
+  }
+
+  getGithubUsername() {}
+
+  getRole() {}
+}
+
+module.exports = Engineer;
---
+++ src/Manager.js
@@ -0,0 +1,16 @@
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+  constructor(name, id, email, officeNumber) {
+    super(name, id, email);
+
+    this.officeNumber = officeNumber;
+    this.role = "Manager";
+  }
+
+  getOfficeNumber() {}
+
+  getRole() {}
+}
+
+module.exports = Manager;
